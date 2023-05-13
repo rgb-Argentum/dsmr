2cb2dac2a6c2bd1713f188c150aa0314a3850ef0
rgb-Argentum <rgbxrgb@gmail.com>
2023-05-13 18:35:01 +0200

    ESPHome component modified: added hungarian extensions

--- components/dsmr/text_sensor.py
+++ components/dsmr/text_sensor.py
@@ -1,85 +1,37 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_SENSOR,
-    ICON_EMPTY,
-    UNIT_WATT_HOURS,
-)
-from . import DSMR, CONF_DSMR_ID
+
+from . import Dsmr, CONF_DSMR_ID
 
 AUTO_LOAD = ["dsmr"]
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_DSMR_ID): cv.use_id(DSMR),
-        cv.Optional("identification"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("p1_version"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("p1_version_be"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("timestamp"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("electricity_tariff"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("electricity_failure_log"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("message_short"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("message_long"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("gas_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("thermal_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("water_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
-        cv.Optional("slave_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
-            }
-        ),
+        cv.GenerateID(CONF_DSMR_ID): cv.use_id(Dsmr),
+        cv.Optional("identification"): text_sensor.text_sensor_schema(),
+        cv.Optional("p1_version"): text_sensor.text_sensor_schema(),
+        cv.Optional("p1_version_be"): text_sensor.text_sensor_schema(),
+        cv.Optional("timestamp"): text_sensor.text_sensor_schema(),
+        cv.Optional("electricity_tariff"): text_sensor.text_sensor_schema(),
+        cv.Optional("electricity_failure_log"): text_sensor.text_sensor_schema(),
+        cv.Optional("message_short"): text_sensor.text_sensor_schema(),
+        cv.Optional("message_long"): text_sensor.text_sensor_schema(),
+        cv.Optional("gas_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("thermal_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("water_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("sub_equipment_id"): text_sensor.text_sensor_schema(),
+        cv.Optional("gas_delivered_text"): text_sensor.text_sensor_schema(),
+        cv.Optional("cosem_device_name"): text_sensor.text_sensor_schema(),
+        cv.Optional("serial_number"): text_sensor.text_sensor_schema(),
+        cv.Optional("circuit_breaker_state"): text_sensor.text_sensor_schema(),
+        cv.Optional("last_month_packed_values"): text_sensor.text_sensor_schema(),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
-def to_code(config):
-    hub = yield cg.get_variable(config[CONF_DSMR_ID])
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_DSMR_ID])
 
     text_sensors = []
     for key, conf in config.items():
@@ -87,11 +39,12 @@ def to_code(config):
             continue
         id = conf.get("id")
         if id and id.type == text_sensor.TextSensor:
-            var = cg.new_Pvariable(conf[CONF_ID])
-            yield text_sensor.register_text_sensor(var, conf)
+            var = await text_sensor.new_text_sensor(conf)
             cg.add(getattr(hub, f"set_{key}")(var))
             text_sensors.append(f"F({key})")
 
-    cg.add_define(
-        "DSMR_TEXT_SENSOR_LIST(F, sep)", cg.RawExpression(" sep ".join(text_sensors))
-    )
+    if text_sensors:
+        cg.add_define(
+            "DSMR_TEXT_SENSOR_LIST(F, sep)",
+            cg.RawExpression(" sep ".join(text_sensors)),
+        )
